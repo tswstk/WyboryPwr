a819af46b637ba2295a082755ed221bd3696d4dd
przemek <przemek1qaz@gmail.com>
2017-01-22 14:57:56 +0100

    Elector JSON Worker JSON

--- app/src/main/java/pl/pwr/wybory/Model/Candidate.java
+++ app/src/main/java/pl/pwr/wybory/Model/Candidate.java
@@ -1,5 +1,10 @@
 package pl.pwr.wybory.Model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -14,6 +19,33 @@ public class Candidate extends Worker {
         super(first_name, last_name, pesel, employmentDate);
         this.program = program;
     }
+
+    public Election(JSONObject jsonString){
+        try {
+            this.program = jsonString.getString("IdKoordynatora");
+            this.positionId = jsonString.getInt("IdStanowiska");
+            this.coordinator = jsonString.getInt("IdKoordynatora");
+            JSONObject positionObject = jsonString.getJSONObject("Stanowisko");
+            this.position = new Position(positionObject.getInt("IdStanowiska"), positionObject.getString("NazwaStanowiska"), positionObject.getString("Wydzial"));
+
+
+            String dateString = jsonString.getString("DataWyborów");
+            dateString = dateString.substring(0, 10);
+
+            //ParsePosition pos = new ParsePosition(0);
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date d = formatter.parse(dateString);
+            /*
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(d);
+            dateString = cal.get(cal.YEAR) + "-" + cal.get(cal.MONTH)  + "-" + cal.get(cal.DAY_OF_MONTH);
+            d = formatter.parse(dateString);
+
+            */
+            this.dateOfElection = d;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     public String getProgram() {
         return program;
