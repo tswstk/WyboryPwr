2c3515909b97254e28e22295ae9d900ae2c86b38
przemek <przemek1qaz@gmail.com>
2017-01-22 15:43:42 +0100

    Candidate JSON

--- app/src/main/java/pl/pwr/wybory/Model/Candidate.java
+++ app/src/main/java/pl/pwr/wybory/Model/Candidate.java
@@ -1,19 +1,23 @@
 package pl.pwr.wybory.Model;
 
+import android.os.Parcelable;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.jar.Pack200;
 
 /**
  * Created by Tomek on 21.01.2017.
  */
 
-public class Candidate extends Worker {
+public class Candidate extends Worker implements Parcelable{
 
     String program;
+    Worker worker;
 
     public Candidate(String first_name, String last_name, long pesel, String employmentDate, String program) throws ParseException {
         super(first_name, last_name, pesel, employmentDate);
@@ -21,8 +25,7 @@ public class Candidate extends Worker {
     }
 
     public Candidate(JSONObject jsonString) throws JSONException, ParseException {
-        super(jsonString.getString("Imie"), jsonString.getString("Nazwisko"), jsonString.getLong("Pesel"), jsonString.getString(("DataZatr")));
-
+        super(jsonString.getJSONObject("Pracownik"));
         try {
             this.program = jsonString.getString("Program");
         } catch (JSONException e) {
